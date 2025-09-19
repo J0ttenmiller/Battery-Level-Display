23f8a03a275edcce16924ada2dadc7819bb488c1
JOttenmiller <104744807+J0ttenmiller@users.noreply.github.com>
2025-09-19 11:49:04 -0700

    early work

---
+++ theme.py
@@ -0,0 +1,16 @@
+OVERLAY_QSS = """
+QLabel {
+    background: rgba(0, 0, 0, 150);
+    color: white;
+    font-size: 14px;
+    padding: 8px;
+    border-radius: 8px;
+}
+"""
+
+MAIN_QSS = """
+QWidget {
+    font-family: Segoe UI, Roboto, Arial;
+    font-size: 12px;
+}
+"""
